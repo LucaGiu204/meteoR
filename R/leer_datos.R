2dfe561e485ddb18b0885a9a74319533708fa88e
LucaGiu204 <lucagiu204@gmail.com>
2025-11-03 11:15:49 -0300

    corrijo funcion leer_estacion y agrego su documentacion

--- R/leer_datos.R
+++ R/leer_datos.R
@@ -1,3 +1,13 @@
+#' Leer datos de una estacion meteorologica
+#'
+#' @param id el id de la estacion a leer
+#' @param ruta ruta donde se descargan o leen los datos
+#'
+#' @returns un dataframe con los datos de la estacion en la ruta indicada
+#' @export
+#'
+#' @examples
+#'   leer_estacion("NH0472", "datos/NH0472.csv")
 leer_estacion <- function(id, ruta){
   url <- glue::glue("https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/{id}.csv")
 
@@ -8,6 +18,6 @@ leer_estacion <- function(id, ruta){
   } else {
     cli::cli_inform("el archivo ya existe en esa direccion, leyendo archivo...")
   }
-  datos <- readr::read.csv(ruta)
-  cli::cli_inform("archivo leido correctamente, el dataset tiene {nrow(datos)} filas y {ncol(datos)} columnas")
+  datos <- readr::read_csv(ruta)
+  cli::cli_inform("archivo leido correctamente,")
   }
