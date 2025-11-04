f7a1eeec811f3ee300cde04a500c2617da1775b2
LucaGiu204 <lucagiu204@gmail.com>
2025-11-04 14:19:30 -0300

    corrijo funcion de graficos

--- R/grafico_temperatura_mensual.R
+++ R/grafico_temperatura_mensual.R
@@ -1,9 +1,9 @@
-grafico_temperatura_mensual <- function(datos, colores = c("blue", "red"), titulo = "Temperatura") {
+grafico_temperatura_mensual <- function(datos, colores = c("blue", "red"), titulo = "Temperatura Promedio Mensual") {
   datos |>
     dplyr::mutate(mes = lubridate::month(fecha)) |>
-    dplyr::group_by(estacion, mes) |>
+    dplyr::group_by(id, mes) |>
     dplyr::summarise(promedio = mean(temperatura_abrigo_150cm, na.rm = TRUE), .groups = "drop") |>
-    ggplot2::ggplot(ggplot2::aes(x = mes, y = promedio, color = estacion)) +
+    ggplot2::ggplot(ggplot2::aes(x = mes, y = promedio, color = id)) +
     ggplot2::geom_line() +
     ggplot2::scale_color_manual(values = colores) +
     ggplot2::labs(title = titulo)
