77e61addd02df0270b7b04777141e08213372581
LucaGiu204 <lucagiu204@gmail.com>
2025-11-04 14:29:49 -0300

    modifico funcion de tabla

--- R/tabla_resumen_temperatura.R
+++ R/tabla_resumen_temperatura.R
@@ -1,15 +1,18 @@
 tabla_resumen_temperatura <- function(datos) {
   datos |>
-    dplyr::select(estacion, temperatura_abrigo_150cm_maxima, temperatura_abrigo_150cm_minima) |>
+    dplyr::select(id, temperatura_abrigo_150cm_maxima, temperatura_abrigo_150cm_minima) |>
     tidyr::pivot_longer(cols = c(temperatura_abrigo_150cm_maxima, temperatura_abrigo_150cm_minima),
                  names_to = "tipo",
                  values_to = "valor") |>
-    dplyr::group_by(estacion) |>
-    dplyr::summarise(media = mean(valor, na.rm = TRUE),
-              desvio = sd(valor, na.rm = TRUE)) |>
+    dplyr::group_by(id) |>
+    dplyr::summarise(
+      media = mean(valor, na.rm = TRUE),
+      desvio = sd(valor, na.rm = TRUE),
+      maximo = max(valor, na.rm = TRUE),
+      minimo = min(valor, na.rm = TRUE))|>
     tidyr::pivot_longer(cols = c(media, desvio),
                  names_to = "estadistica",
                  values_to = "valor") |>
-    tidyr::pivot_wider(names_from = estacion, values_from = valor)
+    tidyr::pivot_wider(names_from = id, values_from = valor)
 }
 
