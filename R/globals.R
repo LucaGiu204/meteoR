dbb6792b07f56636d25627752a67c5e35d6a4f53
LucaGiu204 <lucagiu204@gmail.com>
2025-11-05 21:13:16 -0300

    declaro variables globales con utils::... para corregir la ultima NOTE

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c(
+  "fecha", "id", "mes", "temperatura_abrigo_150cm", "promedio",
+  "temperatura_abrigo_150cm_maxima", "temperatura_abrigo_150cm_minima",
+  "valor", "media", "desvio", "maximo", "minimo", "estadistica"
+))
